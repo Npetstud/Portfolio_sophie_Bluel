fc239a1b0187c52c0b9d699a33b193949c29dc46
Npetstud <npetit80@laposte.net>
2023-10-02 22:45:38 +0200

    SophieBluel Front End Filtre/Gallery
    Après pas mal de recherche et de beaucoups de changement j'ai trouvé pour moi ma solution plus facile afin d'obtenir ce résultat page qui se rafraichit en fonction du filtre demandé.
    Prochaine étape coder la page de connexion

--- FrontEnd/js/home.js
+++ FrontEnd/js/home.js
@@ -1,173 +1,96 @@
 "use strict";
+// Méthode concluante pour créer la page dynamique qui se raffraichit pour chaque click sur les filtres
 
-const works = await fetch("http://localhost:5678/api/works").then(pieces => pieces.json());
+const apiWorks = "http://localhost:5678/api/works";
+fetch(apiWorks)
+  .then(response => response.json())
+  .then(works => {
+    toutPortfolio(works)
+    galleryPhotos = works
+  });
+const apiCategories = "http://localhost:5678/api/categories";
+fetch(apiCategories)
+.then(response => response.json())
+.then(categories => {
+  touteCategories(categories);
+});
 
-
-for (let i = 0; i < works.length; i++) {
-
-    const article = works[i];
-
-const gallery = document.querySelector(".gallery");
-
-const worksElement = document.createElement("article");
-const imageElement = document.createElement("img");
-img.setAttribute("crossorigin", "anonymous");
-img.setAttribute("src", article.imageUrl);
-img.setAttribute("alt", article.title);
-
-const elementOfImage = document.createElement ("element")
-elementOfImage.setAttribute("data-tag", article.category.name);
-elementOfImage.setAttribute("data-id", article.id);
-
-const titleElement = document.createElement ("p")
-titleElement.innerText = article.title;
-
-gallery.appendChild(worksElement);
-worksElement.appendChild(imageElement);
-worksElement.appendChild(elementOfImage);
-worksElement.appendChild(titleElement);
+let galleryPhotos = []
 
 
 
-// //fonction qui crée un élément "element" dans la galerie
-//     const element = document.createElement("element");
-//     element.setAttribute("data-tag", element.category.name);
-//     element.setAttribute("data-id", element.id);
+function toutPortfolio (works) {  
+    let photo = document.querySelector(".gallery");
+    
+    photo.innerHTML = "";
 
-//     const img = document.createElement("img");
-//     img.setAttribute("crossorigin", "anonymous");
-//     img.setAttribute("src", element.imageUrl);
-//     img.setAttribute("alt", element.title);
+    
 
-//     const photoTitle = document.createElement("photoTitle");
-//     photoTitle.innerText = element.title;
+    for (let i = 0; i < works.length; i++){
+        let figure = document.createElement("figure");
+        let img = document.createElement("img");
+        let figcaption = document.createElement("figcaption");
 
-//     element.appendChild(img);
-//     element.appendChild(photoTitle);
+        img.setAttribute("src", works[i].imageUrl);
+        figcaption.setAttribute("alt", works[i].title);
+        
 
-//     gallery.appendChild(element);
-};
+        figcaption.innerHTML = works[i].title;
 
-// fetch("http://localhost:5678/api/works")
-//     .then((res) => {
-//         if (res.ok) {
-//             return res.json();
-//         }
-//     })
+        
+        photo.appendChild(figure);
+        figure.append(img, figcaption);
+    }
+      
+    }
 
-//     //récupération de chaque élément et effectue la fonction "createGallery" pour chaque élément récupéré
-//     .then((photos) => {
-//         photos.forEach((photo) => {
-//             createGallery(photo);
-//         });
-//     });
+    function touteCategories (categories) {  
+    
+      let filtre = document.querySelector(".filtre");
+  
+      filtre.innerHTML = "";
 
-    //fonction qui crée un bouton dans la nav #filtres
-const createButton = (button) => {
-    //on récupère le parent des boutons
-    const filtres = document.querySelector("#filtres");
-    //on ajoute le bouton
-    filtres.insertAdjacentHTML(
-        "beforeend",
-        `<button data-tag="${button.name}"> ${button.name} </button>`
-    );
-};
+      
+let allButton = document.createElement("button");  
+      allButton.innerHTML = "Tous";
+      filtre.appendChild(allButton);
+      allButton.addEventListener("click", function () {
+        toutPortfolio(galleryPhotos);
+        
+      });
+    
+      for (let i = 0 ; i < categories.length; i++){
 
-fetch("http://localhost:5678/api/categories")
-    //si fetch fonctionne on récupère les données au format JSON
-    .then((res) => {
-        if (res.ok) {
-            return res.json();
-        }
-    })
+      
 
-    //récupération de chaque data et effectue la fonction "createButton" pour chaque data récupéré
-    .then((data) => {
-        data.forEach((button) => {
-            createButton(button);
-        });
-    })
 
-    .then(() => {
-        //on récupère les boutons
-        const buttons = document.querySelectorAll("#filtres button");
-        //pour chaque bouton, si on clique dessus, fais une fonction
-        buttons.forEach((button) => {
-            button.addEventListener("click", function () {
-                //on enlève le classe "active" à tout les boutons
-                buttons.forEach((element) =>
-                    element.classList.remove("active")
-                );
-                //on met la classe "active" au bouton cliqué
-                this.classList.add("active");
+        let button = document.createElement("button");
+        button.setAttribute("value", categories[i].name);
+        button.innerHTML = categories[i].name;
+        filtre.appendChild(button);
+        button.addEventListener("click", function (){
+        const filterphoto = galleryPhotos.filter(function(photo){
+                
+                console.log(categories[i].id, photo.categoryId)
 
-                //on récupère le tag du bouton
-                const buttonTag = this.dataset.tag;
-                //on récupère les element faites plus haut
-                const images = document.querySelectorAll(".gallery element");
-                //pour chaque element on fait une fonction
-                images.forEach((image) => {
-                    //si l'image a le même tag que le bouton, on enlève la classe "filtered"
-                    if (image.getAttribute("data-tag") === buttonTag) {
-                        image.classList.remove("filtered");
-                        //on applique la classe "filtered" aux autres qui n'ont pas le même tag
-                    } else {
-                        image.classList.add("filtered");
-                    }
-                });
-            });
-        });
+                return photo.categoryId === categories[i].id
+              });
+            
+              toutPortfolio(filterphoto)
+            
 
-        //fonction qui va afficher toutes les images
-        function afficherToutesLesImages() {
-            //récupère toutes les images
-            const toutesLesImages =
-                document.querySelectorAll(".gallery element");
-            //on enlève la classe "filtered" a chaque images
-            toutesLesImages.forEach((image) => {
-                image.classList.remove("filtered");
-            });
-        }
-        //quand on clique sur "Tous", va faire la fonction afficherToutesLesImages
-        boutonTous.addEventListener("click", afficherToutesLesImages);
-    });
-
-//permet d'ajouter la classe active au bouton "Tous" par défaut
-const boutonTous = document.querySelector('button[data-tag="Tous"]');
-boutonTous.classList.add("active");
+        }) 
+        
+}
+}
 
 
 
 
 
 
+// Seconde méthode non concluante n'ayant pas réussi a creer un filtre interactf
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// "use strict";
 
 // const galleryPhotos = document.querySelector(".gallery");
 
@@ -197,35 +120,34 @@ boutonTous.classList.add("active");
 
 //  const filtreGallery = document.querySelector("#filtres");
 
-//  const filterInfo = function(data2){
+//  const filterInfo = function(button){
 //     const html =`
-//     <button class="btn-filtrer">${data2.name}`
+//     <button data-tag="${button.name}"> ${button.name} </button>`
 //     filtreGallery.insertAdjacentHTML ("beforeend", html)
-//  };
-
-
-
+//  }
+//  const buttons = document.querySelectorAll("#filtres button");
+//             //pour chaque bouton, si on clique dessus, fais une fonction
+//             buttons.forEach((button) => {
+//                 button.addEventListener("click", function () {
+//                     //on enlève le classe "active" à tout les boutons
+//                   for
+//                         }
+//                     )});
+                
+            
+    
+           
+ 
  
 //  const infoFilter = async function(){
 //      const res = await fetch("http://localhost:5678/api/categories");
 //      console.log(res);
-//      const data2= await res.json();
-//      console.log(data2);
-//      for (const displayInsidefonction of data2){
+//      const data= await res.json();
+//      console.log(data);
+//      for (const displayInsidefonction of data){
 //         filterInfo(displayInsidefonction)
 //      }
 //  }
 //  infoFilter();
 
-//  const boutonFiltrer = document.querySelector(".btn-filtrer");
-// boutonFiltrer.addEventListener("click", function () {
-
-    
-// });
-
-
-
-
-
-
-
+//
