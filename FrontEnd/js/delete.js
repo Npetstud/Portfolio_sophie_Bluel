fa24df0d65887b7ac87ce09a4814c9f9b72023d1
Npetstud <npetit80@laposte.net>
2023-10-26 22:13:31 +0200

    Modal ajout/suppression
    Maintenant j'ai la possiblité d'ajouter et supprimer une photo.
    Revoir le css
    et revoir si possibilité de mettre l'ajout et la suppression dans un autre fichier.

---
+++ FrontEnd/js/delete.js
@@ -0,0 +1,29 @@
+// trashIcon.addEventListener('click', function() {
+//     fetch("http://localhost:5678/api/works/{id}", {
+//       method: 'DELETE',
+//       headers: {
+//         Authorization : 'Bearer ' + tokenJeton,
+//         'Content-Type': 'application/json'
+//       }
+//     })
+//     .then(response => response.json())
+//     .then(data => {
+//       figure.remove();
+//     })
+    
+    
+//   });
+
+
+
+
+
+
+
+  // function deleteWorks(){
+//     console.log("hello");
+// }
+
+// deleteWorks();
+
+// export default{deleteWorks};
--- FrontEnd/js/modal.js
+++ FrontEnd/js/modal.js
@@ -1,3 +1,5 @@
+// import deleteWorks from "./delete.js";
+
 const openModal = function(e) {
     e.preventDefault();
     const target = document.querySelector(e.target.getAttribute("href"));
@@ -43,27 +45,145 @@ const openModal = function(e) {
   function renderProjectModal(works) {
     const modalProject = document.querySelector(".project-modal");
     modalProject.innerHTML = "";
-    
+    const tokenJeton = window.localStorage.getItem("jeton")    
     const projectModal = document.querySelector('.project-modal');
   
     for (let i = 0; i < works.length; i++) {
       const figure = document.createElement("figure");
       const img = document.createElement("img");
       const figcaption = document.createElement("figcaption");
+      const trashIcon = document.createElement('i');
+      
+    
+    
       
   
   
       img.setAttribute("src", works[i].imageUrl);
       figcaption.setAttribute("alt", works[i].title);
+      trashIcon.classList.add('fa-solid', 'fa-trash-can');
       
       
-     
-  
-   
+
+      trashIcon.addEventListener('click', function() {
+        const projectId = works[i].id;
+        fetch(apiWorks + '/' + projectId, {
+          method: 'DELETE',
+          headers: {
+            Authorization : 'Bearer ' + tokenJeton,
+            'Content-Type': 'application/json'
+          }
+        })
+        .then(response => response.json())
+        .then(data => {
+          figure.remove(data);
+          
+        })
+        
+        
+      });
   
       projectModal.appendChild(figure);
-      figure.append(img, figcaption);
+      figure.append(img, figcaption, trashIcon);
   
       
     }
   }
+
+
+const ajouterPhotoBtn = document.querySelector('.js-ajouter-photo');
+const modal1 = document.getElementById('modal1');
+const modal2 = document.getElementById('modal2');
+const backArrowBtn = document.querySelector('.fa-arrow-left');
+
+ajouterPhotoBtn.addEventListener('click', () => {
+
+// Passage de modal1 a modal2
+
+  modal2.style.display = 'flex';
+  modal1.style.display = 'none'; 
+
+});
+
+backArrowBtn.addEventListener('click', () => {
+
+ // Passage de modal2 a modal1
+
+  modal1.style.display = 'flex';
+  modal2.style.display = 'none';
+});
+
+// AJOUTER UN PROJET
+
+
+const form = document.getElementById('project-form');
+const fileInput = document.getElementById('file-upload');
+const tokenJeton = window.localStorage.getItem('jeton');
+
+
+// affichage de la prévisualisation de l'image sélectionnée
+fileInput.addEventListener('change', function(event) {
+  const file = event.target.files[0];
+  if (file) {
+    const previewImg = document.createElement('img');
+    const previewContainer = document.createElement('div');
+    const projectModal2 = document.querySelector('.project-modal2')
+
+    const reader = new FileReader();
+
+    reader.onload = function(event) {
+      previewImg.setAttribute('src', event.target.result);
+      previewContainer.appendChild(previewImg);
+      form.insertBefore(previewContainer, form.childNodes[0]);
+      
+    }
+
+    reader.readAsDataURL(file);
+    projectModal2.style.display = 'none';
+    
+    
+  }
+});
+
+// soumission du formulaire
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  // récupération des valeurs du formulaire
+  const title = document.getElementById('title').value;
+  const category = document.getElementById('category').value;
+  const file = document.getElementById('file-upload').files[0];
+
+  // Validation des données du formulaire
+  if (!title || !category || !file) {
+    console.error('Veuillez remplir tous les champs du formulaire');
+    return;
+  }
+  
+  
+
+  // création de l'objet FormData pour l'envoi du formulaire
+  const formData = new FormData();
+  formData.append('title', title);
+  formData.append('category', category);
+  formData.append('image', file);
+
+  // envoi de la requête pour l'ajout de la photo dans la liste de projets
+  fetch('http://localhost:5678/api/works', {
+    method: 'POST',
+    headers: {
+      Authorization : 'Bearer ' + tokenJeton,
+      
+    },
+    body: formData,
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    // réinitialisation du formulaire et de la prévisualisation de l'image
+    form.reset();
+    previewContainer.innerHTML = '';
+  })
+  .catch(error => console.error('Erreur lors de l\'ajout du projet :', error));
+  });
+  
